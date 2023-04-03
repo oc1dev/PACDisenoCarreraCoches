3ea5846dae4710b1a9d41b20c7d9d71d31db316a
oc1navarro <oc1.navarro@gmail.com>
2023-04-03 10:39:04 +0200

    v2

--- scripts/Jqscript.js
+++ scripts/Jqscript.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
+    let participantes_num = $("#cars").val();
+    let podium = [];
+    var ancho_pantalla = $(window).width();
+    var lineaMeta = ancho_pantalla - 160;
+
+
     //cargamos Array con coches segun numero de participantes
 
     $("#cars").change(function () {
         //primero limpiamos por si ya existian pistas
         $(".pistaCarreras").remove();
         //Obtenemos numero de participantes
-        let participantes_num = $("#cars").val();
+        participantes_num = $("#cars").val();
         //declaramos array a rellenar segun ejercicio
         let array_coches = [];
         //rellenamos array
@@ -29,29 +35,46 @@ $(document).ready(function () {
 
     //ahora vamos con la animacion de los coches
 
-    $("#arrancar").click(function () {
-        //ancho de la pantalla
-        var ancho_pantalla = $(window).width();
 
-        //ancho del coche
-        var ancho_coche = $(".coche1").width();
-        //longitud a recorrer restamos tb el ancho de la meta
-        var lineaMeta = ancho_pantalla - ancho_coche-10;
-        
+    $("#arrancar").click(function prueba() {
+        //ancho de la pantalla
+
+
+
+        //longitud a recorrer 
+
+
         //definimos numero de participantes y array para podium
-        let participantes_num = $("#cars").val();
-        
-        let podium = [];
-        podium.length
+
+
+
+
         //avance de cada coche
-       // while (podium.length < participantes_num) {
-            for (let i = 1; i <= participantes_num; i++) {
-                if (($(".coche"+i).position().left) >= lineaMeta) { continue }
-                let tirada = Math.floor(Math.random() * 10);
-                //let marcha = parseInt(tirada) + parseInt($(".coche" + i).css("left"));
-                $(".coche" + i).animate({ "left":"+="+tirada+"px"}, 3);
-                if (parseInt($(".coche" + i).css("left")) >= lineaMeta) { podium.push("car" + i) };
-            }
-        //};
+        podium=[];
+        avanceCoches();
+
+
+
+
     });
+    function avanceCoches() {
+        for (let i = 1; i <= participantes_num; i++) {
+
+            let tirada = Math.floor(Math.random() * 10) + 1;
+            let tiempo = tirada * 75;
+            $(".coche" + i).animate({ "left": lineaMeta+"px" }, tiempo, llegaMeta(i));
+
+
+        }
+
+    }
+
+    function llegaMeta(i) {
+        podium.push("car" + i + ".png");
+        if (podium.length==participantes_num){
+            $("#prueba").html(podium);
+        }
+    }
+
+
 });
